3df7f42e4d2fff3441938c75a6b0371fc5c1ff31
Skylar Kelty <skylarkelty@gmail.com>
2013-12-07 13:31:21 +0000

    Add autoloader support

--- php-c.php
+++ php-c.php
@@ -3,6 +3,12 @@
  * Provides a basic shell, with some added extras
  */
 
+// If there is an auto loader here, include it
+if (file_exists("vendor/autoload.php")) {
+	echo "Loading autoloader...\n";
+	include("vendor/autoload.php");
+}
+
 // Basic Shell
 $fp = fopen("php://stdin", "r");
 $in = '';
